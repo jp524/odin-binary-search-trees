bf8d79f3c1da5313cf43fede3dc27043ca3d2aab
JP <85654561+jp524@users.noreply.github.com>
2022-08-26 15:20:22 -0400

    Build build_tree method for tree with one level

--- bst.rb
+++ bst.rb
@@ -1,8 +1,10 @@
 class Node
+  attr_accessor :data, :left, :right
+
   include Comparable
 
-  def initialize
-    @data = []
+  def initialize(data)
+    @data = data
     @left = []
     @right = []
   end
@@ -13,19 +15,31 @@ class Node
 end
 
 class Tree
+  attr_reader :root
+
   def initialize(array)
     @array = array
+    clean_array
     @root = ''
+    @start = 0
+    @finish = @array.size - 1
   end
 
   def clean_array
     @array.sort!.uniq!
   end
 
-  def build_tree
-    clean_array
+  def build_tree(array = @array, start = @start, finish = @finish)
+    return nil if start > finish
+
+    mid = (start + finish) / 2
+    node = Node.new(array[mid])
+    node.left = array[start..mid - 1]
+    node.right = array[mid + 1..finish]
+    @root = node
   end
 end
 
-tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p tree.build_tree
+tree = Tree.new([1, 2, 3])
+tree.build_tree
+p tree.root
